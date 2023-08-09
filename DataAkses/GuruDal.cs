57e506c498e6470c8e57ec7c8dd39b5fe5e648cc
FadlanBM <104897512+FadlanBM@users.noreply.github.com>
2023-08-09 08:41:27 +0700

    add_show_data

---
+++ DataAkses/GuruDal.cs
@@ -0,0 +1,99 @@
+﻿using Dapper;
+using Nuna.Lib.DataAccessHelper;
+using sekolahku_jude.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sekolahku_jude.DataAkses
+{
+    public class GuruDal
+    {
+        private string _conn;
+        public GuruDal() {
+            _conn = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+        }
+
+        public void Insert(GuruModel model)
+        {
+            //  QUERY
+            const string sql = @"
+                INSERT INTO tb_guru VALUES (@GuruId, @GuruName)";
+
+            //  PARAM
+            var dp = new DynamicParameters();
+            dp.AddParam("@GuruId", model.GuruId, System.Data.SqlDbType.VarChar);
+            dp.AddParam("@GuruName", model.GuruName, System.Data.SqlDbType.VarChar);
+
+            //  EXECUTE
+            using (var conn = new SqlConnection(_conn))
+            {
+                conn.Execute(sql, dp);
+            }
+
+        }
+        public void Update(GuruModel model)
+        {
+            //  QUERY
+            const string sql = @"
+                Update 
+                tb_guru
+                set 
+                guru_name=@GuruName
+                where 
+                guru_id=@GuruId";
+
+            //  PARAM
+            var dp = new DynamicParameters();
+            dp.AddParam("@GuruId", model.GuruId, System.Data.SqlDbType.VarChar);
+            dp.AddParam("@GuruName", model.GuruName, System.Data.SqlDbType.VarChar);
+
+            //  EXECUTE
+            using (var conn = new SqlConnection(_conn))
+            {
+                conn.Execute(sql, dp);
+            }
+        }
+
+        public void Delete(GuruModel model)
+        {
+            const string sql = @"Delete Form tb_guru where guru_id=@GuruId";
+
+
+            var dp = new DynamicParameters();
+            dp.AddParam("@GuruID", model.GuruId, System.Data.SqlDbType.VarChar);
+
+            using (var conn = new SqlConnection(_conn))
+            {
+                conn.Execute(sql, dp);
+            }
+        }
+
+        public GuruModel getData(string id)
+        {
+            const string sql = @"select * from tb_guru where guru_id=@GuruId";
+
+            var dp = new DynamicParameters();
+            dp.AddParam("@GuruId", id, System.Data.SqlDbType.VarChar);
+
+            using (var conn = new SqlConnection(_conn))
+            {
+                return conn.ReadSingle<GuruModel>(sql, dp);
+            }
+        }
+
+        public IEnumerable<GuruModel> ListData()
+        {
+            const string sql = @"select guru_id, guru_name from tb_guru";
+
+            using (var conn = new SqlConnection(_conn))
+            {
+                return conn.Read<GuruModel>(sql);
+            }
+        }
+    }
+}
--- Forms/GuruForm.cs
+++ Forms/GuruForm.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using sekolahku_jude.DataAkses;
+using sekolahku_jude.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +14,84 @@ namespace sekolahku_jude.Forms
 {
     public partial class GuruForm : Form
     {
+        AppDbContextDataContext context;
+        private GuruDal dal;
         public GuruForm()
         {
+            context = new AppDbContextDataContext();
+            dal = new GuruDal();
             InitializeComponent();
+        }
+
+        private void loadData() { 
+            dataGridView1.RowHeadersVisible = false;
+            dataGridView1.AllowUserToAddRows = false;
+         
+            var data =(from c in context.tb_gurus
+                       orderby c.guru_id ascending
+                       select c).ToList();
+            var binding = new BindingSource();
+            binding.DataSource = data;
+            dataGridView1.DataSource = binding;
+
+        }
+
+        private void clearForm() {
+            tb_guruId.Text = "";
+            tb_guruName.Text = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (tb_guruId.Text == "" || tb_guruName.Text =="")
+            {
+                MessageBox.Show(null, "Form Tidak Boleh kosong", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (tb_guruId.Text.Length>3)
+            {
+                MessageBox.Show(null, "Input harus di bawah 3 kata", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (tb_guruName.Text.Length>30)
+            {
+                MessageBox.Show(null, "Input harus di bawah 30 kata", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            var guru = new GuruModel
+            {
+                GuruId = tb_guruId.Text,
+                GuruName = tb_guruName.Text,
+            };
+
+            var gurudb = dal.getData(guru.GuruId);
+            if (gurudb != null)
+            {
+                dal.Update(guru);
+                MessageBox.Show(null, "Berhasil Update data","Information",MessageBoxButtons.OK,MessageBoxIcon.Information);
+                loadData();
+               clearForm();
+            }
+            else
+            {
+                dal.Insert(guru);
+                MessageBox.Show(null, "Berhasil Insert data", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                loadData();
+                clearForm();
+            }
+        }
+        
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void GuruForm_Load(object sender, EventArgs e)
+        {
+            loadData();
         }
     }
 }
