4edca07fcb54634d8745ffb4a674971048b56165
FadlanBM <104897512+FadlanBM@users.noreply.github.com>
2023-08-12 15:03:48 +0700

    menambahkan_update

--- Forms/GuruForm.Designer.cs
+++ Forms/GuruForm.Designer.cs
@@ -29,13 +29,18 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.tb_guruname = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.tb_guruId = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btn_edit = new System.Windows.Forms.DataGridViewButtonColumn();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
@@ -43,45 +48,21 @@
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.Cornsilk;
-            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.tb_guruname);
             this.panel1.Controls.Add(this.label2);
-            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.tb_guruId);
             this.panel1.Controls.Add(this.label1);
             this.panel1.Location = new System.Drawing.Point(242, 12);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(233, 324);
             this.panel1.TabIndex = 0;
             // 
-            // dataGridView1
+            // tb_guruname
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(224, 353);
-            this.dataGridView1.TabIndex = 1;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(3, 12);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(47, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Guru ID";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(6, 28);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(224, 22);
-            this.textBox1.TabIndex = 1;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(6, 67);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(224, 22);
-            this.textBox2.TabIndex = 3;
+            this.tb_guruname.Location = new System.Drawing.Point(6, 67);
+            this.tb_guruname.Name = "tb_guruname";
+            this.tb_guruname.Size = new System.Drawing.Size(224, 22);
+            this.tb_guruname.TabIndex = 3;
             // 
             // label2
             // 
@@ -92,14 +73,45 @@
             this.label2.TabIndex = 2;
             this.label2.Text = "Guru Name";
             // 
+            // tb_guruId
+            // 
+            this.tb_guruId.Location = new System.Drawing.Point(6, 28);
+            this.tb_guruId.Name = "tb_guruId";
+            this.tb_guruId.Size = new System.Drawing.Size(224, 22);
+            this.tb_guruId.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Guru ID";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.btn_edit});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(224, 353);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(316, 342);
+            this.button1.Location = new System.Drawing.Point(243, 342);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 2;
             this.button1.Text = "Save";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -109,6 +121,40 @@
             this.button2.TabIndex = 3;
             this.button2.Text = "Delete";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(320, 342);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Clear";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "No";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Guru Id";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Guru Name";
+            this.Column3.Name = "Column3";
+            // 
+            // btn_edit
+            // 
+            this.btn_edit.HeaderText = "Action";
+            this.btn_edit.Name = "btn_edit";
+            this.btn_edit.Text = "Edit";
+            this.btn_edit.ToolTipText = "m";
+            this.btn_edit.UseColumnTextForButtonValue = true;
             // 
             // GuruForm
             // 
@@ -116,6 +162,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Khaki;
             this.ClientSize = new System.Drawing.Size(487, 377);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView1);
@@ -123,6 +170,7 @@
             this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Name = "GuruForm";
             this.Text = "Guru";
+            this.Load += new System.EventHandler(this.GuruForm_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
@@ -134,11 +182,16 @@
 
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox tb_guruname;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox tb_guruId;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewButtonColumn btn_edit;
     }
 }
--- Forms/GuruForm.cs
+++ Forms/GuruForm.cs
@@ -12,9 +12,136 @@ namespace sekolahku_jude.Forms
 {
     public partial class GuruForm : Form
     {
+        AppDbContextDataContext context;
         public GuruForm()
         {
+            context = new AppDbContextDataContext();
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (tb_guruId.Text == "" || tb_guruname.Text == "")
+            {
+                MessageBox.Show(null, "Form harus di isi secara lengkap", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            var data = context.tb_gurus.Where(g => g.guru_id == tb_guruId.Text).FirstOrDefault();
+
+            if (data == null)
+            {
+                tb_guru guru = new tb_guru();
+                guru.guru_id = tb_guruId.Text;
+                guru.guru_name = tb_guruname.Text;
+                context.tb_gurus.InsertOnSubmit(guru);
+                context.SubmitChanges();
+                MessageBox.Show(null, "Berhasil menginput data", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                cleareData();
+                loadData();
+                return;
+            }
+            else
+            {
+                data.guru_name = tb_guruname.Text;
+                context.SubmitChanges();
+                MessageBox.Show(null, "Update data menginput data", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                cleareData();
+                loadData();
+                return;
+            }
+
+        }
+
+        private void loadData()
+        {
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.RowHeadersVisible = false;
+            dataGridView1.Rows.Clear();
+            int i = 0;
+            var data = (from c in context.tb_gurus
+                        orderby c.guru_name ascending
+                        select new
+                        {
+                            guru_id = c.guru_id,
+                            guru_name = c.guru_name
+                        }).ToList();
+            foreach (var item in data)
+            {
+                var num = dataGridView1.Rows.Add();
+                i++;
+                dataGridView1.Rows[num].Cells[0].Value = i;
+                dataGridView1.Rows[num].Cells[1].Value = item.guru_id;
+                dataGridView1.Rows[num].Cells[2].Value = item.guru_name;
+            }
+
+        }
+
+        private void GuruForm_Load(object sender, EventArgs e)
+        {
+            loadData();
+        }
+
+        private void cleareData() {
+            tb_guruId.Text = "";
+            tb_guruname.Text = "";
+            tb_guruId.Enabled = true;
+            tb_guruname.Enabled = true;
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            string id = dataGridView1.Rows[e.RowIndex].Cells[1].Value.ToString();
+            if (e.ColumnIndex == 3)
+            {
+                var data = context.tb_gurus.Where(g => g.guru_id == id).FirstOrDefault();
+                tb_guruId.Text = data.guru_id;
+                tb_guruname.Text = data.guru_name.ToString();
+                tb_guruId.Enabled = false;
+            }
+
+        }
+
+        private void disableTB() {
+
+            tb_guruId.Enabled = false;
+            tb_guruname.Enabled = false;
+        }
+        private void enabelTb()
+        {
+            tb_guruname.Enabled = true;
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            cleareData();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            if (tb_guruId.Text!="")
+            {
+                var data = context.tb_gurus.Where(g => g.guru_id == tb_guruId.Text).FirstOrDefault();                
+                disableTB();
+                DialogResult dialogResult = MessageBox.Show(null, "Apakah Anda yakin ingin menghapus data ini ?", "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+                if (dialogResult == DialogResult.Yes)
+                {
+                    context.tb_gurus.DeleteOnSubmit(data);
+                    context.SubmitChanges();
+                    MessageBox.Show(null, "Berhasil Menghapus data !", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    cleareData();
+                    loadData();
+                }
+                else
+                {
+                    enabelTb();
+                }
+            }
+            else
+            {
+                    MessageBox.Show(null, "Pilih dulu data yang mau di uodate", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    enabelTb();
+            }
         }
     }
 }
